c33e9c0abf5ec998c7297680159e690477d93e95
pjanks <johnnykcassidy@gmail.com>
2021-10-21 09:13:43 -0700

    Adjust scoring

--- resources/views/components/game-instructions-modal.blade.php
+++ resources/views/components/game-instructions-modal.blade.php
@@ -1,6 +1,6 @@
 <div class="game-instructions-modal hidden">
     <p class="game-instructions-paragraph">
-      The game can be started by clicking the 'Start' button. Upon 'GAME OVER', the board can be reset by clicking the 'Reset' button, and a new game can be intialized by, again, clicking the 'Start' button. You can move left with the 'A' key, up with the 'W' key, right with the 'D' key, and down with the 'S' key. The objective is to consume as many of the pills as possible without colliding with the walls or with your own tail. You will be declared a winner after you accumulate 1500 pills. Good luck!
+      The game can be started by clicking the 'Start' button. Upon 'GAME OVER', the board can be reset by clicking the 'Reset' button, and a new game can be intialized by, again, clicking the 'Start' button. You can move left with the 'A' key, up with the 'W' key, right with the 'D' key, and down with the 'S' key. The objective is to consume as many of the pills as possible without colliding with the walls or with your own tail. You will be declared a winner after you accumulate 1250 pills. Good luck!
     </p>
     <button class="close-instructions-button game-button">Close</button>
     <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
